8ff437c8b6b5673ea62c0b788fd289462af25318
vatchalaramesh16@gmail.com <vatchalaramesh16@gmail.com>
2026-05-18 18:21:04 +0530

    Helloooooo

--- src/com/model/Inventory.java
+++ src/com/model/Inventory.java
@@ -1,36 +1,40 @@
 package com.model;
 
-
-
 public class Inventory {
 
-    private String packageinventoryId;
-    private String productId;
-    private int    quantityInStock;
-    private LocalDateTime lastUpdated;
-
-   
-
-    public Inventory(String inventoryId, String productId,
-                     int quantityInStock, LocalDateTime lastUpdated) {
-        this.inventoryId     = inventoryId;
-        this.productId       = productId;
-        this.quantityInStock = quantityInStock;
-        this.lastUpdated     = lastUpdated;
-    }
-
-    // Getters and Setters
-    public String getInventoryId()                  { return inventoryId; }
-    public void setInventoryId(String inventoryId)  { this.inventoryId = inventoryId; }
-
-    public String getProductId()                    { return productId; }
-    public void setProductId(String productId)      { this.productId = productId; }
-
-    public int getQuantityInStock()                 { return quantityInStock; }
-    public void setQuantityInStock(int qty)         { this.quantityInStock = qty; }
-
-    public LocalDateTime getLastUpdated()           { return lastUpdated; }
-    public void setLastUpdated(LocalDateTime dt)    { this.lastUpdated = dt; }
-
-   
+//    private String packageinventoryId;
+//    private String productId;
+//    private int    quantityInStock;
+//    private LocalDateTime lastUpdated;
+//
+//   
+//
+//    public Inventory(String inventoryId, String productId,
+//                     int quantityInStock, LocalDateTime lastUpdated) {
+//        this.inventoryId     = inventoryId;
+//        this.productId       = productId;
+//        this.quantityInStock = quantityInStock;
+//        this.lastUpdated     = lastUpdated;
+//    }
+//
+//    // Getters and Setters
+//    public String getInventoryId()                  { return inventoryId; }
+//    public void setInventoryId(String inventoryId)  { this.inventoryId = inventoryId; }
+//
+//    public String getProductId()                    { return productId; }
+//    public void setProductId(String productId)      { this.productId = productId; }
+//
+//    public int getQuantityInStock()                 { return quantityInStock; }
+//    public void setQuantityInStock(int qty)         { this.quantityInStock = qty; }
+//
+//    public LocalDateTime getLastUpdated()           { return lastUpdated; }
+//    public void setLastUpdated(LocalDateTime dt)    { this.lastUpdated = dt; }
+//
+//   
+//}
+	
+public static void main(String args[])
+{
+	System.out.println("Hello World!!!!!");
+}
 }
