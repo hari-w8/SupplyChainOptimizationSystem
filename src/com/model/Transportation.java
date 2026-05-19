3e724cc2d4cf0f71aa80da2994f4b41486c4f742
mahal <mahal@192.168.1.66>
2026-05-19 09:39:34 +0530

    Merge branch 'master' of https://github.com/hari-w8/SupplyChainOptimizationSystem.git

---
+++ src/com/model/Transportation.java
@@ -0,0 +1,51 @@
+package com.model;
+
+public class Transportation
+{
+	private String shipmentId;
+	private String orderId;
+	private String carrierId;
+	private String status;
+
+
+
+public Transportation(String shipmentId, String orderId, String carrierId, String status) {
+	//super();
+	this.shipmentId = shipmentId;
+	this.orderId = orderId;
+	this.carrierId = carrierId;
+	this.status = status;
+}
+
+public String getShipmentId() {
+	return shipmentId;
+}
+
+public void setShipmentId(String shipmentId) {
+	this.shipmentId = shipmentId;
+}
+
+public String getOrderId() {
+	return orderId;
+}
+
+public void setOrderId(String orderId) {
+	this.orderId = orderId;
+}
+
+public String getCarrierId() {
+	return carrierId;
+}
+
+public void setCarrierId(String carrierId) {
+	this.carrierId = carrierId;
+}
+
+public String getStatus() {
+	return status;
+}
+
+public void setStatus(String status) {
+	this.status = status;
+}
+}
