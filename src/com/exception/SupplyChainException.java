46a62e5851511a21317fe5255a5e846d90d24851
hari-w8 <harii0211hh@gmail.com>
2026-05-20 12:15:07 +0530

    updated

--- src/com/exception/SupplyChainException.java
+++ src/com/exception/SupplyChainException.java
@@ -1,29 +1,10 @@
 package com.exception;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> branch 'master' of https://github.com/hari-w8/SupplyChainOptimizationSystem
 
->>>>>>> branch 'master' of https://github.com/hari-w8/SupplyChainOptimizationSystem
-public class SupplyChainException extends Exception {
-
-    
-    private static final long serialVersionUID = 1L;
-
-    
-    public SupplyChainException(String message) {
-        super(message);  
-        }
-
-    public SupplyChainException(String message, Throwable cause) {
-        super(message, cause);
-=======
 public class SupplyChainException extends Exception {
 
     public SupplyChainException(String message) {
         super(message);
->>>>>>> branch 'master' of https://github.com/hari-w8/SupplyChainOptimizationSystem.git
+
     }
 }
